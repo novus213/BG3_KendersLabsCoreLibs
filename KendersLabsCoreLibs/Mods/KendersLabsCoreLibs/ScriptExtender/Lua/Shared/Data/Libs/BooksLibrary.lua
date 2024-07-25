fca0316810de684b38228f985ce6975a05bf3126
Novus213 <44229417+novus213@users.noreply.github.com>
2024-07-25 21:57:07 +0200

    init empty table

---
+++ KendersLabsCoreLibs/Mods/KendersLabsCoreLibs/ScriptExtender/Lua/Shared/Data/Libs/BooksLibrary.lua
@@ -0,0 +1,5 @@
+Data.Libs.Dnd5eLimited = {}
+Data.Libs.Dnd5e = {}
+Data.Libs.Dnd5eExtended = {}
+Data.Libs.Flavours = {}
+Data.Libs.Legacy = {}
--- KendersLabsCoreLibs/Mods/KendersLabsCoreLibs/ScriptExtender/Lua/Shared/Data/Libs/_Init.lua
+++ KendersLabsCoreLibs/Mods/KendersLabsCoreLibs/ScriptExtender/Lua/Shared/Data/Libs/_Init.lua
@@ -1,3 +1,4 @@
+Libs = {}
 RequireFiles("Shared/Data/Libs/", {
     "BooksLibrary",
     "ClassesLibrary",
